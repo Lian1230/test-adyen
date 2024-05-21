4ecef93f6770f0081527f9944a84b3209903dc27
Lian Longfeng <lianlongfeng@gmail.com>
2024-05-21 08:10:17 -0400

    u
    add suspense

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,5 +2,5 @@ export const POST = async (req: Request) => {
   const body = await req.json();
   console.log("body", JSON.stringify(body));
 
-  return new Response(JSON.stringify({ message: "Webhook received" }));
+  return new Response(JSON.stringify({ message: "Webhook received", body }));
 };
